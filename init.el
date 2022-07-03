672cca77d3f25cdcac2ac875a31582608ebfcbdd
Benjamin Stürz <benni@stuerz.xyz>
2022-07-03 19:39:14 +0200

    Updated init.el
    Signed-off-by: Benjamin Stürz <benni@stuerz.xyz>

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
-
 (scroll-bar-mode -1)            	; Disable visible scrollbar
 (tool-bar-mode -1)              	; Disable the toolbar
 (tooltip-mode -1)               	; Disable tooltips
 (set-fringe-mode 10)            	; Give some breathing room
 (menu-bar-mode -1)              	; Disable the menu bar
-(setq visible-bell t)           	; Set up the visible bell
+(setq ring-bell-function 'ignore)	; Disable the bell
 (setq inhibit-startup-message t)	; Don't display the startup screen
-(load-theme 'wombat)			; Set the theme
+(setq scroll-step 1)			; Make scrolling smoother
 
+; Use ESC instead of tripple-ESC
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (require 'package)			; Initialize package sources
@@ -26,7 +26,16 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(use-package command-log-mode)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes.
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda() (display-line-numbers-mode 0))))
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -45,4 +54,96 @@
   :config
   (ivy-mode 1))
 
-(use-package counsel)
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; Use the Doom Modeline.
+(use-package all-the-icons
+  :if (display-graphic-p))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 10))
+
+(use-package doom-themes
+  :init (load-theme 'doom-palenight t))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.5))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package general
+  :config
+  (general-create-definer benni/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC"))
+
+  (benni/leader-keys
+    "o"  '(counsel-find-file :which-key "open file")
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme"))
+
+(general-define-key "C-M-j" 'counsel-switch-buffer)
+(general-define-key "C-x M-t" 'counsel-load-theme)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  ;:hook (evil-mode . benni/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers.
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+(use-package hydra)
+
+; Text scaling (TODO: Add keybind)
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("+" text-scale-increase "in")
+  ("-" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(benni/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
